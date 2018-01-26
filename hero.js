9422722f076df729a0ee0a03939056ab7bc626bc
fraserbrown87 <fraserbrown87@gmail.com>
2018-01-26 15:34:29 +0000

    initial commit

---
+++ hero.js
@@ -0,0 +1,11 @@
+var Hero = require('./hero.js');
+
+var Hero function(name, health favFood, health) {
+  this.name = name;
+  this.favFood = favFood;
+  this.health = health;
+  this.tasks = []
+
+}
+
+module.exports = Hero;
